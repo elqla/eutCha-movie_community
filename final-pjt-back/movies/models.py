cbb74b77a6f27f736a0148273b912490bf8dcbf0
elqla <elqla19@gmail.com>
2022-05-20 16:16:03 +0900

    create_moviemodel

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Genre(models.Model):
+    genre = models.CharField(max_length=10000)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    credits = models.CharField(max_length=1000)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_movies")
+    genres = models.ManyToManyField(Genre, related_name="movie")   ###array[integer]
+    release_date = models.CharField(max_length=100)
+    popularity = models.FloatField()
+    movie_id = models.IntegerField()
+    poster_url = models.URLField(max_length=1000)
+
---
+++ final-pjt-back/movies/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+from .models import Movie, Genre
+
+User = get_user_model()
+
+class MovieSerializer(serializers.ModelSerializer):
+    class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = User
+            fields = ('pk', 'username')
+    class GenreSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Genre
+            fields = '__all__'
+    like_users = UserSerializer(read_only=True, many=True)
+    genres = GenreSerializer(many=True)
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class MovieSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Movie
+            fields = ('title',)
+    movies = MovieSerializer(read_only=True, many=True)
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
