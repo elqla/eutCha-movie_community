c56d9a639ddc1628d14bd2f85e72b2fce54958e1
lu1um <emperor1812@gmail.com>
2022-05-23 23:17:14 +0900

    add_movidDetail

---
+++ final-pjt-front/src/views/MovieDetailView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="movie-detail">
+    {{ $route.params.movieId }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieDetailView',
+}
+</script>
+
+<style>
+.movie-detail {
+  margin-top: 100px;
+}
+</style>
