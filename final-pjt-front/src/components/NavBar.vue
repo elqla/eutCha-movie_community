52ddd1bfb88d492e6eba54fee9174f258c3377d7
lu1um <emperor1812@gmail.com>
2022-05-23 15:38:50 +0900

    fix_HomeView

--- final-pjt-front/src/components/NavBar.vue
+++ final-pjt-front/src/components/NavBar.vue
@@ -1,36 +1,17 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
-
-          <router-link to="/">Home</router-link> |
-          <router-link to="/login">Login</router-link>
-
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
+    <b-navbar type="dark" variant="info"
+      class="d-flex justify-content-between"
+    >
+      <b-navbar-brand href="#">eutCha</b-navbar-brand>
+      <b-navbar-nav>
+        <a>welcome, eutCha</a>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <router-link to="/">Home</router-link> | 
+        <router-link to="/login">Login</router-link> | 
+        <a>my Profile</a>
+      </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
--- final-pjt-front/src/views/HomeView.vue
+++ final-pjt-front/src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <router-link to="/login" class="gate">
       <img src="../assets/title.png" alt="Home title">
+      <p>안녕하세요</p>
     </router-link>
   </div>
 </template>
@@ -14,14 +15,17 @@ export default {
 </script>
 
 <style>
-  /* body {
-    background: #000;
-  } */
+@import url('https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&family=Noto+Sans+KR&display=swap');
 
   .gate {
     display: flex;
     justify-content: center;
     align-items: center;
+    background-color: black;
     min-height: 100vh;
+  }
+
+  p {
+    font-family: 'Nanum Myeongjo', serif;
   }
 </style>
