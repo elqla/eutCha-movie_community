f24b664341c6b729f8961e531a4d79b68feea010
elqla <elqla19@gmail.com>
2022-05-23 15:30:33 +0900

    signup_ing

--- final-pjt-front/src/router/index.js
+++ final-pjt-front/src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
+import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import LogoutView from '@/views/LogoutView.vue'
+import SignupView from '@/views/SignupView.vue'
+import NotFound404 from '@/views/NotFound404.vue'
 
 Vue.use(VueRouter)
 
@@ -15,7 +18,32 @@ const routes = [
     path: '/login',
     name: 'login',
     component: LoginView
-  }
+  },
+  {
+    path: '/logout',
+    name: 'logout',
+    component: LogoutView
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignupView
+  },
+  // {
+  //   path: 
+  //   name: 
+  //   component: 
+  // },
+  {
+    path: '/404',
+    name: 'NotFound404',
+    component: NotFound404
+  },
+  {
+    path: '*',
+    redirect: '/404'
+  },
+
 ]
 
 const router = new VueRouter({
