c61ce5ae31aae4a7a5b4f9c426b30be92aecdab7
lu1um <emperor1812@gmail.com>
2022-05-23 14:01:18 +0900

    add_navbar

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link>
-    </nav>
+    <nav-bar></nav-bar>
     <router-view/>
   </div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'App', 
+  component: {
+    NavBar,
+  }
+}
+</script>
 
 <style>
 #app {
