3554111f90151f43ce3c149b617fbe86dc1a3f4c
lu1um <emperor1812@gmail.com>
2022-05-24 09:16:16 +0900

    fix_navbar

--- final-pjt-front/src/components/NavBar.vue
+++ final-pjt-front/src/components/NavBar.vue
@@ -3,7 +3,9 @@
     <b-navbar type="white"
       class="d-flex justify-content-between"
     >
-      <b-navbar-brand href="#">eutCha</b-navbar-brand>
+      <router-link to="/movies">
+      <b-navbar-brand>eutCha</b-navbar-brand>
+      </router-link>
       <b-navbar-nav class="welcome">
         <a>welcome, eutCha</a>
       </b-navbar-nav>
@@ -11,9 +13,7 @@
         <router-link to="/movies">Home</router-link> | 
         <router-link to="/login" v-if="!isLoggedIn">Login</router-link> | 
         <router-link to="/logout" v-if="isLoggedIn">Logout</router-link> | 
-        <li v-if="isLoggedIn">
         <router-link :to="{ name: 'profile', params: { username } }">profile</router-link>
-        </li>
       </b-navbar-nav>
     </b-navbar>
   </div>
