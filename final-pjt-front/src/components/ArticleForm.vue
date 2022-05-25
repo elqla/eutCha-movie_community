2956e9d4d9bc37d0261642e0693ae936e81eb08d
lu1um <emperor1812@gmail.com>
2022-05-25 09:11:10 +0900

    form

--- final-pjt-front/src/components/ArticleForm.vue
+++ final-pjt-front/src/components/ArticleForm.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
     <h1>FORM</h1>
-    <b-form @submit.prevent="newArticle(credentials, moviePk)" align="left">
+    <p>{{credentials.moviePk}}</p>
+    <b-form @submit.prevent="newArticle(credentials)" align="left">
       <b-form-group label-for="title" label="Title">
-        <b-form-input v-model="credentials.title" id="title" placeholder="제목" type="text" required>
+        <b-form-input v-model="credentials.article.title" id="title" placeholder="제목" type="text" required>
         </b-form-input>
       </b-form-group>
       <br>
       <!--  -->
       <b-form-group label-for="content" label="Content">
-        <b-form-input v-model="credentials.content" id="content" placeholder="내용" type="text" required>
+        <b-form-input v-model="credentials.article.content" id="content" placeholder="내용" type="text" required>
         </b-form-input>
       </b-form-group>
       <br>
@@ -26,10 +27,12 @@ export default {
   data(){
     return {
       credentials:{
-        title : this.payload.title,
-        content : this.payload.content,
+        article: {
+          title : this.payload.title,
+          content : this.payload.content,
+        },
+        moviePk: this.payload.movie.id
       },
-      moviePk: this.payload.movie.id
     }
   },
   props: {
